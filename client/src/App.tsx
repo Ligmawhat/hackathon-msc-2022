d5408e20d950ce054edfaaf2949cc7c3a837e685
Алексей Рыбалкин <llllll99999@MacBook-Air-Aleksej.local>
2022-06-10 16:19:13 +0300

    auth JWT front

---
+++ client/src/App.tsx
@@ -0,0 +1,58 @@
+import React, { FC, useEffect, useContext, useState } from "react";
+import LoginForm from "./components/LoginForm";
+import { Context } from "./index";
+import { IUser } from "./models/IUser";
+import { observer } from "mobx-react-lite";
+import UserService from "./services/UserService";
+
+const App: FC = () => {
+  const { store } = useContext(Context);
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      store.checkAuth();
+    }
+  }, []);
+
+  async function getUsers() {
+    try {
+      const response = await UserService.fetchUsers();
+      setUsers(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  if (store.isLoading) {
+    return <div>Загрузка...</div>;
+  }
+
+  if (!store.isAuth) {
+    return (
+      <div>
+        <LoginForm />
+        <button onClick={getUsers}>Получить пользователей</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>
+        {store.isAuth
+          ? `Пользователь авторизован ${store.user.email}`
+          : "АВТОРИЗУЙТЕСЬ"}
+      </h1>
+      <button onClick={() => store.logout()}>Выйти</button>
+      <div>
+        <button onClick={getUsers}>Получить пользователей</button>
+      </div>
+      {users.map((user) => (
+        <div key={user.email}>{user.email}</div>
+      ))}
+    </div>
+  );
+};
+
+export default observer(App);
