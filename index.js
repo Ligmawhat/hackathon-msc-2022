9c6efb22d9dad14ed2efd8955decf3c26e38fe7b
Aleksey Mityukov <alekseymityukov@Alekseys-MacBook-Pro.local>
2022-06-10 16:58:05 +0300

    delete front

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { User, Sequelize } = require("./db/models");
-const router = require("./router/index");
+const router = require("./router");
 const errorMiddleware = require("./middlewares/error-middleware");
 const path = require('path')
 
