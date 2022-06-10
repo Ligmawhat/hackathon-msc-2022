0ad3990b575bd5e8bbd4f209f9a21158fd6ac31d
Aleksey Mityukov <alekseymityukov@Alekseys-MacBook-Pro.local>
2022-06-10 17:10:17 +0300

    backend ready

--- server.js
+++ server.js
@@ -22,17 +22,13 @@ app.use(express.static(path.join(__dirname, 'build')))
 
 app.use("/api", router);
 app.use(errorMiddleware);
-const start = async () => {
-  try {
-      app.get('/*', (req, res) => {
-          // res.sendFile(path.join((__dirname, 'build', 'index.html')))
-          res.sendFile('./build/index.html', {root: __dirname})
-      })
-    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-  } catch (e) {
-    console.log(e);
-  }
-};
-// comment
 
-start();
+app.get('/*', (req, res) => {
+    try {
+        res.sendFile('./build/index.html', {root: __dirname})
+        // res.sendFile(path.join((__dirname, 'build', 'index.html')))
+    } catch (e) {
+        console.log(e)
+    }
+      })
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
