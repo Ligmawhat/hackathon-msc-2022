3fd716082bd2b8198b7587740953cbb93f0a1e47
darsen <dlya_web_konkursa@mail.ru>
2022-06-12 20:22:52 +0300

    merged skts

--- server.js
+++ server.js
@@ -1,63 +1,65 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const { User, Sequelize } = require('./db/models');
-const router = require('./router');
-const errorMiddleware = require('./middlewares/error-middleware');
-const path = require('path');
-const http = require('http');
-const WebSocket = require('ws');
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const {User, Sequelize} = require('./db/models')
+const router = require('./router')
+const errorMiddleware = require('./middlewares/error-middleware')
+const path = require('path')
+const http = require('http')
+const WebSocket = require('ws')
 
-const PORT = 3001;
-const app = express();
-const registerWsEmitter = require('./src/ws/wsEmitter');
-const map = new Map();
+const PORT = 3001
+const app = express()
+const registerWsEmitter = require('./src/ws/wsEmitter')
+const myEmitter = require('./src/ee')
+const {SEND_MESSAGE_SOCKET} = require('./src/constants/event')
+const map = new Map()
 
-app.use(express.json());
-app.use(cookieParser());
+app.use(express.json())
+app.use(cookieParser())
 app.use(
-  cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
-  })
-);
-app.use(express.static(path.join(__dirname, 'build')));
+    cors({
+        credentials: true,
+        origin: process.env.CLIENT_URL,
+    })
+)
+app.use(express.static(path.join(__dirname, 'build')))
 
-app.use('/api', router);
-app.use(errorMiddleware);
+app.use('/api', router)
+app.use(errorMiddleware)
 
 app.get('/*', (req, res) => {
-  try {
-    res.sendFile('./build/index.html', { root: __dirname });
-    // res.sendFile(path.join((__dirname, 'build', 'index.html')))
-  } catch (e) {
-    console.log(e);
-  }
-});
+    try {
+        res.sendFile('./build/index.html', {root: __dirname})
+        // res.sendFile(path.join((__dirname, 'build', 'index.html')))
+    } catch (e) {
+        console.log(e)
+    }
+})
 
 // comment
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ clientTracking: false, noServer: true });
+const server = http.createServer(app)
+const wss = new WebSocket.Server({clientTracking: false, noServer: true})
 
 //1
 server.on('upgrade', function (request, socket, head) {
-  console.log('Session is parsed!');
-  wss.handleUpgrade(request, socket, head, function (ws) {
-    wss.emit('connection', ws, request);
-  });
-});
+    console.log('Session is parsed!')
+    wss.handleUpgrade(request, socket, head, function (ws) {
+        wss.emit('connection', ws, request)
+    })
+})
 
-registerWsEmitter(map);
+registerWsEmitter(map)
 
 //2
 wss.on('connection', function (ws, request) {
-  const userId = request.user.id;
-  map.set(userId, ws);
+    const userId = request?.user?.id
+    map.set(userId, ws)
 
-  ws.on('close', function () {
-    map.delete(userId);
-  });
-});
+    ws.on('close', function () {
+        map.delete(userId)
+    })
+})
 
-server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`))
